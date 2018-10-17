7ef2e99acb4b8b4fa5079e13023ceb0d83b54bc3
Paweł Gesing <pawel.gesing@onwelo.com>
2018-10-17 14:53:04 +0200

    #16 CYValidator + tests

---
+++ src/main/java/com/onwelo/tinvalidator/validator/CYValidator.java
@@ -0,0 +1,26 @@
+package com.onwelo.tinvalidator.validator;
+
+import java.util.regex.Pattern;
+
+public class CYValidator implements Validator {
+
+    private static final Pattern PATTERN = Pattern.compile("[0-9]{8}[A-Z]");
+
+    public Pattern getPattern(){
+        return PATTERN;
+    }
+
+    public boolean computeControlSum(String tin) {
+        int[] weights = {1, 0, 5, 7, 9, 13, 15, 17, 19, 21};
+        try {
+            int sum = 0;
+            for (int i = 0; i < 8; i++) {
+                int d = Integer.parseInt(tin.substring(i, i + 1));
+                sum += ((i+1)%2==0 ? d : weights[d]);
+            }
+            return ((sum % 26) + 65) == tin.charAt(8);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
