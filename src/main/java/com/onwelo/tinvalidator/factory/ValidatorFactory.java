a876b77b52ed5f4d97a5165ff3e5f7f61f863354
Paweł Gesing <pawel.gesing@onwelo.com>
2018-10-17 10:39:49 +0200

    #1 separation of junit version; path to constant; ValidatorFactory tests

--- src/main/java/com/onwelo/tinvalidator/factory/ValidatorFactory.java
+++ src/main/java/com/onwelo/tinvalidator/factory/ValidatorFactory.java
@@ -5,11 +5,13 @@ import com.onwelo.tinvalidator.validator.Validator;
 
 public class ValidatorFactory {
 
+    private static final String PACKAGE_PATH = "com.onwelo.tinvalidator.validator.";
+
     public static Validator findValidatorForCountryCode(String code) throws NoSuchValidatorException {
         try {
-            return (Validator) Class.forName("com.onwelo.tinvalidator.validator." + code + "Validator").newInstance();
+            return (Validator) Class.forName(PACKAGE_PATH + code + "Validator").newInstance();
         } catch (Exception e){
-            throw new NoSuchValidatorException("Validator for country code: " + code + "cannot be fetched.", e);
+            throw new NoSuchValidatorException("Validator for country code: " + code + " cannot be fetched.", e);
         }
     }
 }
---
+++ src/test/java/com/onwelo/tinvalidator/factory/ValidatorFactoryTest.java
@@ -0,0 +1,34 @@
+package com.onwelo.tinvalidator.factory;
+
+import com.onwelo.tinvalidator.exception.NoSuchValidatorException;
+import com.onwelo.tinvalidator.validator.PLValidator;
+import com.onwelo.tinvalidator.validator.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+@DisplayName("ValidatorFactory should")
+public class ValidatorFactoryTest {
+
+    @Test
+    @DisplayName("throw exception on instantiating Validator")
+    void throwExceptionOnValidatorInstantiating(){
+        Executable exe = () -> ValidatorFactory.findValidatorForCountryCode("");
+        Assertions.assertThrows(NoSuchValidatorException.class, exe);
+    }
+
+    @Test
+    @DisplayName("throw exception on not existing country code")
+    void throwExceptionOnNotExistingCountryCode(){
+        Executable exe = () -> ValidatorFactory.findValidatorForCountryCode("XX");
+        Assertions.assertThrows(NoSuchValidatorException.class, exe);
+    }
+
+    @Test
+    @DisplayName("fetch PLValidator by 'PL' string")
+    void PLValidatorFetchingTest() throws Exception{
+        Validator v = ValidatorFactory.findValidatorForCountryCode("PL");
+        Assertions.assertTrue(v instanceof PLValidator);
+    }
+}
