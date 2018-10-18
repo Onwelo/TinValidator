9ac6a551d73e3e1cc789cb02521effcd3aa2966f
Michał Bil <michal.bil@onwelo.com>
2018-10-18 10:26:36 +0200

    CZRValidator + Tests

---
+++ src/main/java/com/onwelo/tinvalidator/validator/CZRValidator.java
@@ -0,0 +1,26 @@
+package com.onwelo.tinvalidator.validator;
+
+import java.util.regex.Pattern;
+
+public class CZRValidator implements Validator{
+
+    private static final Pattern PATTERN = Pattern.compile("([0-9]{6})+(|/?)+([0-9]{3})+([0-9]?)");
+
+    public Pattern getPattern(){
+        return PATTERN;
+    }
+
+    public boolean computeControlSum(String tin) {
+        try {
+            String tinWithoutSlash = tin.replace("/", "");
+            int month = Integer.parseInt(tinWithoutSlash.substring(2,4));
+            int day = Integer.parseInt(tinWithoutSlash.substring(4,6));
+            assert (month >= 1 && month <= 12) || (month >= 21 && month <= 32)
+                    || (month >= 51 && month <= 62) || (month >= 71 && month <= 82);
+            assert day >= 1 && day <= 31;
+            return true;
+        } catch (NumberFormatException | AssertionError e) {
+            return false;
+        }
+    }
+}
