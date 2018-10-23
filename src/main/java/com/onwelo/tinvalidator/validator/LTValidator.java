8c8e942c11d78cb2476da0535976a77251355709
Paweł Gesing <pawel.gesing@onwelo.com>
2018-10-23 11:29:30 +0200

    #34 corrected (implemented) LTValidator + tests

--- src/main/java/com/onwelo/tinvalidator/validator/LTValidator.java
+++ src/main/java/com/onwelo/tinvalidator/validator/LTValidator.java
@@ -1,5 +1,38 @@
 package com.onwelo.tinvalidator.validator;
 
-public class LTValidator extends EEValidator {
-    //validation is identical to Estonian one
+import java.util.regex.Pattern;
+
+public class LTValidator implements Validator {
+
+    private static final Pattern PATTERN = Pattern.compile("\\d{9}|\\d{10}1\\d");
+
+    public Pattern getPattern(){
+        return PATTERN;
+    }
+
+    public boolean computeControlSum(String tin) {
+        int[] weights = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+        int tinLength = tin.length();
+        try {
+            int sum = 0;
+            for (int i = 0; i < tinLength - 1; i++) {
+                sum += Integer.parseInt(tin.substring(i, i + 1)) * weights[i];
+            }
+            if (sum % 11 != 10) {
+                return (sum % 11) == Integer.parseInt(tin.substring(tinLength - 1, tinLength));
+            }
+
+            int[] weights2 = {3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4};
+            sum = 0;
+            for (int i = 0; i < tinLength - 1; i++) {
+                sum += Integer.parseInt(tin.substring(i, i + 1)) * weights2[i];
+            }
+            if (sum % 11 != 10) {
+                return (sum % 11) == Integer.parseInt(tin.substring(tinLength - 1, tinLength));
+            }
+            return 0 == Integer.parseInt(tin.substring(tinLength - 1, tinLength));
+        } catch (NumberFormatException|AssertionError e) {
+            return false;
+        }
+    }
 }
