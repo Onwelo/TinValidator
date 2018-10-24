8434034a183c8a49af1b44ce20e85d0f057ced13
Bartosz Sądel <bartosz.sadel@onwelo.com>
2018-10-24 09:38:24 +0200

    #49 - code review changes

--- src/main/java/com/onwelo/tinvalidator/validator/SEValidator.java
+++ src/main/java/com/onwelo/tinvalidator/validator/SEValidator.java
@@ -11,21 +11,23 @@ public class SEValidator implements Validator {
     }
 
     public boolean computeControlSum(String tin) {
-        int r1 = Integer.parseInt(tin.substring(11));
-        if (r1 == 0 || r1 > 94) {
+        try {
+            int r1 = Integer.parseInt(tin.substring(11));
+            assert r1 > 0 && r1 < 95;
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                int element = Integer.parseInt(tin.substring(i, i + 1));
+                if (i % 2 == 1) {
+                    sum += element;
+                } else {
+                    sum += element / 5 + ((element * 2) % 10);
+                }
+            }
+
+            return (10 - (sum % 10)) % 10 == Integer.parseInt(tin.substring(10, 11));
+        } catch (AssertionError e) {
             return false;
         }
-
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            int elemtent = Integer.parseInt(tin.substring(i, i + 1));
-            if (i % 2 == 1) {
-                sum += elemtent;
-            } else {
-                sum += elemtent / 5 + ((elemtent * 2) % 10);
-            }
-        }
-
-        return (10 - (sum % 10)) % 10 == Integer.parseInt(tin.substring(10, 11));
     }
 }
