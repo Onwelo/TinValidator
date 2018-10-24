225d36da00391ce5ba749c0891b0d0fc6812da45
Michał Bil <michal.bil@onwelo.com>
2018-10-24 12:56:25 +0200

    FRValidator + tests

--- src/test/java/com/onwelo/tinvalidator/validator/FRValidatorTest.java
+++ src/test/java/com/onwelo/tinvalidator/validator/FRValidatorTest.java
@@ -6,28 +6,47 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("French TIN should")
-public class FRValidatorTest {
+class FRValidatorTest {
 
 	@ParameterizedTest
-	@DisplayName("contain thirteen digits")
-	@CsvSource(value = { "1111111111, false", "11111111111, false", "1, false", "999999999, false",
-			"0123456789012, true" })
-	void tinShouldContainThirteenDigits(final String tin, final boolean expectedResult) {
+	@DisplayName("contain eleven characters")
+	@CsvSource(value = {
+			"AB234567890, true",
+			"01234567890, true",
+			"AB1111111, false",
+			"DD11111111, false",
+			"1, false",
+			"999999999, false",})
+	void tinShouldContainElevenCharacters(final String tin, final boolean expectedResult) {
 		Validator v = new FRValidator();
 		Assertions.assertEquals(expectedResult, v.matchRegex(tin));
 	}
 
 	@ParameterizedTest
-	@DisplayName("not contain letters")
-	@CsvSource(value = { "1111111111111, true", "111111a111321, false", "x, false" })
-	void tinShouldNotContainLetters(final String tin, final boolean expectedResult) {
+	@DisplayName("not contain letters except first two characters (only capitalized are allowed)")
+	@CsvSource(value = {
+			"AB234567890, true",
+			"CD354124244, true",
+			"FF543678888, true",
+			"ff543678888, false",
+			"XX41234444A, false",
+			"XX41sa44A4S4, false"
+	})
+	void tinShouldNotContainLettersExceptFirstTwoCharacters(final String tin, final boolean expectedResult) {
 		Validator v = new FRValidator();
 		Assertions.assertEquals(expectedResult, v.matchRegex(tin));
 	}
 
 	@ParameterizedTest
 	@DisplayName("compute control sum correctly")
-	@CsvSource(value = { "3023217600053, true", "1230967654123, false", "0001123456789, false" })
+	@CsvSource(value = {
+			"00300076965, true",
+			"03552081317, true",
+			"03784359069, true",
+			"39192312345, false",
+			"94716252522, false",
+			"57772177372, false",
+	})
 	void tinShouldComputeControlSum(final String tin, final boolean expectedResult) {
 		Validator v = new FRValidator();
 		Assertions.assertEquals(expectedResult, v.computeControlSum(tin));
