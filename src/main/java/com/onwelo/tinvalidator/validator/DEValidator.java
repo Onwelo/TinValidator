f9371923eda915dfaf1c7a23a451f5dbbacf915b
marek <marekwbc@gmail.com>
2020-08-11 11:43:31 +0200

    DE control sum - removal of incorrect algorithm implementation

--- src/main/java/com/onwelo/tinvalidator/validator/DEValidator.java
+++ src/main/java/com/onwelo/tinvalidator/validator/DEValidator.java
@@ -4,26 +4,14 @@ import java.util.regex.Pattern;
 
 public class DEValidator implements Validator {
 
-    private static final Pattern PATTERN = Pattern.compile("[1-9]\\d{8}");
+    private static final Pattern PATTERN = Pattern.compile("[0-9]\\d{9}");
 
     public Pattern getPattern(){
         return PATTERN;
     }
 
     public boolean computeControlSum(String tin) {
-        try {
-            int rest = 10;
-            for (int i = 0; i < 8; i++) {
-                rest = (Integer.parseInt(tin.substring(i, i + 1)) + rest) % 10;
-                if (rest == 0){
-                    rest = 10;
-                }
-                rest = rest * 2 % 11;
-            }
-            rest = 11 - rest;
-            return (rest == 10 ? 0 : rest) == Integer.parseInt(tin.substring(8, 9));
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        //TODO zaimplementować nowy algorytm do wyliczania sumy kontrolnej gdy będzie on znany.
+        return true;
     }
 }
